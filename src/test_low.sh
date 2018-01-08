fa4a53d935ceb7d7fd8d8e8c320ee78854d9fdf1
Aaron Faanes <dafrito@gmail.com>
2018-01-07 19:19:06 -0600

    Bump logging level for nc

--- src/test_low.sh
+++ src/test_low.sh
@@ -8,4 +8,4 @@ test $# -ge 2 || die "Usage: $0 <port> <test-name>"
 port=$1
 name=$2
 test -f $name.hreq || die "No $name.hreq test found."
-nc localhost $port --ssl -C --ssl-verify --ssl-trustfile=certificate.pem -c "unix2dos <$name.hreq" -i 5 -o $name.hrep
+nc -vvv localhost $port --ssl -C --ssl-verify --ssl-trustfile=certificate.pem -c "unix2dos <$name.hreq" -o $name.hrep
